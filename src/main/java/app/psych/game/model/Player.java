b74f5444b8e97db7e945118e1a15b2d9e2f7ab63
Ajitesh Kumar <ajiteshkmr802@gmail.com>
2019-12-23 15:44:34 +0530

    init

---
+++ src/main/java/app/psych/game/model/Player.java
@@ -0,0 +1,49 @@
+package app.psych.game.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "players")
+public  class Player implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    @NotBlank
+    private String name;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Getter
+    @Setter
+    private Date createdAt = new Date();
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Getter
+    @Setter
+    private Date updatedAt = new Date();
+
+
+
+
+
+
+
+
+
+}
